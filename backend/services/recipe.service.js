4bee6761206a077a7864c946fa0600c0ed9d5cd8
Mircea Hanghiuc <mirceah99@gmail.com>
2022-06-24 20:40:50 +0300

    temporary solution for tag filter

--- backend/services/recipe.service.js
+++ backend/services/recipe.service.js
@@ -1,7 +1,7 @@
 const {
 	recipes,
 	instructions,
-	ingredientsForRecipe
+	ingredientsForRecipe,
 } = require("../models/index");
 const { usersLikes } = require("../models/index");
 
@@ -122,7 +122,7 @@ exports.getRecipeById = async (encryptedId) => {
 	// calculate total calories
 	total.calories = total.protein * 4 + total.carbs * 4 + total.fat * 9;
 	return total;
-}
+};
 
 exports.getRecipeById = async (encryptedId) => {
 	const decryptedId = decryptId(encryptedId)[0];
@@ -131,7 +131,9 @@ exports.getRecipeById = async (encryptedId) => {
 	let recipe = await recipes.findByPk(decryptedId);
 
 	//get recipe ingredients
-	const ingredients = await IngredientService.getIngredientsByRecipeId(recipe.get().id);
+	const ingredients = await IngredientService.getIngredientsByRecipeId(
+		recipe.get().id
+	);
 
 	//get data for each ingredient and sum macros and micros
 	let total = await this.calculateMacros(ingredients);
@@ -192,12 +194,13 @@ exports.calculateMacros = async (ingredients) => {
 };
 
 exports.getRecipeByDecryptedId = async (decryptedId) => {
-
 	//get recipe main data
 	let recipe = await recipes.findByPk(decryptedId);
 
 	//get recipe ingredients
-	const ingredients = await IngredientService.getIngredientsByRecipeId(recipe.get().id);
+	const ingredients = await IngredientService.getIngredientsByRecipeId(
+		recipe.get().id
+	);
 
 	recipe = recipe.dataValues;
 	let recipeResponse = {
@@ -215,29 +218,31 @@ exports.getRecipeByDecryptedId = async (decryptedId) => {
 
 exports.getRecipesByIngredientId = async (ingredientId) => {
 	let recipes = [];
-	return ingredientsForRecipe.findAll({
-		where: {
-			ingredientId: ingredientId,
-		},
-	})
-		.then(async res => {
+	return ingredientsForRecipe
+		.findAll({
+			where: {
+				ingredientId: ingredientId,
+			},
+		})
+		.then(async (res) => {
 			for (let ingredientForRecipe of res) {
-				let recipe = await this.getRecipeByDecryptedId(ingredientForRecipe.recipeId);
+				let recipe = await this.getRecipeByDecryptedId(
+					ingredientForRecipe.recipeId
+				);
 				recipes.push(recipe);
 			}
 			return recipes;
 		})
-		.catch(err => {
+		.catch((err) => {
 			console.error(err);
 			throw new CustomError("Error while getting recipes by ingredient", 500);
-		})
-}
+		});
+};
 
 exports.updateTotalsOfRecipeByIngredientId = async (ingredientId) => {
 	let recipes = await this.getRecipesByIngredientId(ingredientId);
-	for (let recipe of recipes)
-		this.updateTotalsOfRecipeById(recipe.id);
-}
+	for (let recipe of recipes) this.updateTotalsOfRecipeById(recipe.id);
+};
 
 exports.updateTotalsOfRecipeById = async (recipeId) => {
 	let ingredients = await ingredientsForRecipe.findAll({
@@ -251,9 +256,11 @@ exports.updateTotalsOfRecipeById = async (recipeId) => {
 		totalCarbs: 0,
 		totalFat: 0,
 		totalFiber: 0,
-	}
+	};
 	for (let ingredientForRecipe of ingredients) {
-		let ingredient = await IngredientService.getIngredientById(ingredientForRecipe.ingredientId);
+		let ingredient = await IngredientService.getIngredientById(
+			ingredientForRecipe.ingredientId
+		);
 		totals.totalProtein += ingredient.macros.protein;
 		totals.totalCarbs += ingredient.macros.carbs;
 		totals.totalFat += ingredient.macros.fat;
@@ -266,22 +273,27 @@ exports.updateTotalsOfRecipeById = async (recipeId) => {
 	totals.totalCarbs = totals.totalCarbs * recipe.servingSize;
 	totals.totalFat = totals.totalFat * recipe.servingSize;
 	totals.totalFiber = totals.totalFiber * recipe.servingSize;
-	totals.totalCalories = totals.totalProtein * 4 + totals.totalCarbs * 4 + totals.totalFat * 9;
+	totals.totalCalories =
+		totals.totalProtein * 4 + totals.totalCarbs * 4 + totals.totalFat * 9;
 
-	return await recipes.update({
-		...totals,
-	}, {
-		where: { id: recipeId },
-	})
-		.then(res => {
+	return await recipes
+		.update(
+			{
+				...totals,
+			},
+			{
+				where: { id: recipeId },
+			}
+		)
+		.then((res) => {
 			console.log("Updated recipe", res, " with totals ", totals);
 			return res;
 		})
-		.catch(err => {
+		.catch((err) => {
 			console.error(err);
 			throw new CustomError("Error while updating recipe totals", 500);
-		})
-}
+		});
+};
 
 exports.addImageToRecipe = async (image, recipeId) => {
 	await ImageService.addImageToRecipe(image, recipeId);
@@ -331,121 +343,121 @@ exports.searchRecipes = async (searchOptions) => {
 				name: {
 					[Op.iLike]: `%${searchOptions.filter.name}%`,
 				},
-			}
+			};
 		if (searchOptions.filter.description)
 			queryOptions.where = {
 				description: {
 					[Op.iLike]: `%${searchOptions.filter.description}%`,
 				},
-			}
+			};
 		if (searchOptions.filter.prepTime?.start)
 			queryOptions.where = {
 				prepTime: {
 					[Op.gte]: searchOptions.filter.prepTime.start,
 				},
-			}
+			};
 		if (searchOptions.filter.prepTime?.end)
 			queryOptions.where = {
 				prepTime: {
 					[Op.lte]: searchOptions.filter.prepTime.end,
 				},
-			}
+			};
 		if (searchOptions.filter.cookingTime?.start)
 			queryOptions.where = {
 				cookingTime: {
 					[Op.gte]: searchOptions.filter.cookingTime.start,
 				},
-			}
+			};
 		if (searchOptions.filter.cookingTime?.end)
 			queryOptions.where = {
 				cookingTime: {
 					[Op.lte]: searchOptions.filter.cookingTime.end,
 				},
-			}
+			};
 		if (searchOptions.filter.servingSize?.start)
 			queryOptions.where = {
 				servingSize: {
 					[Op.gte]: searchOptions.filter.servingSize.start,
 				},
-			}
+			};
 		if (searchOptions.filter.servingSize?.end)
 			queryOptions.where = {
 				servingSize: {
 					[Op.lte]: searchOptions.filter.servingSize.end,
 				},
-			}
+			};
 		if (searchOptions.filter.calories?.start)
 			queryOptions.where = {
 				totalCalories: {
 					[Op.gte]: searchOptions.filter.calories.start,
 				},
-			}
+			};
 		if (searchOptions.filter.calories?.end)
 			queryOptions.where = {
 				totalCalories: {
 					[Op.lte]: searchOptions.filter.calories.end,
 				},
-			}
+			};
 		if (searchOptions.filter.protein?.start)
 			queryOptions.where = {
 				totalProtein: {
 					[Op.gte]: searchOptions.filter.protein.start,
 				},
-			}
+			};
 		if (searchOptions.filter.protein?.end)
 			queryOptions.where = {
 				totalProtein: {
 					[Op.lte]: searchOptions.filter.protein.end,
 				},
-			}
+			};
 		if (searchOptions.filter.carbs?.start)
 			queryOptions.where = {
 				totalCarbs: {
 					[Op.gte]: searchOptions.filter.carbs.start,
 				},
-			}
+			};
 		if (searchOptions.filter.carbs?.end)
 			queryOptions.where = {
 				totalCarbs: {
 					[Op.lte]: searchOptions.filter.carbs.end,
 				},
-			}
+			};
 		if (searchOptions.filter.fat?.start)
 			queryOptions.where = {
 				totalFat: {
 					[Op.gte]: searchOptions.filter.fat.start,
 				},
-			}
+			};
 		if (searchOptions.filter.fat?.end)
 			queryOptions.where = {
 				totalFat: {
 					[Op.lte]: searchOptions.filter.fat.end,
 				},
-			}
+			};
 		if (searchOptions.filter.fiber?.start)
 			queryOptions.where = {
 				totalFiber: {
 					[Op.gte]: searchOptions.filter.fiber.start,
 				},
-			}
+			};
 		if (searchOptions.filter.fiber?.end)
 			queryOptions.where = {
 				totalFiber: {
 					[Op.lte]: searchOptions.filter.fiber.end,
 				},
-			}
+			};
 		if (searchOptions.filter.author)
 			queryOptions.where = {
 				author: {
 					[Op.iLike]: `%${searchOptions.filter.author}%`,
 				},
-			}
+			};
 		if (searchOptions.filter.tags)
 			queryOptions.where = {
 				"$tags.text$": {
 					[Op.iLike]: { [Op.any]: searchOptions.filter.tags },
 				},
-			}
+			};
 	}
 	if (searchOptions.order) {
 		queryOptions.order = searchOptions.order;
@@ -456,14 +468,15 @@ exports.searchRecipes = async (searchOptions) => {
 	if (searchOptions.offset) {
 		queryOptions.offset = searchOptions.offset;
 	}
-
+	//TODO FIX THIS TAGS SEARCH
+	/*
 	queryOptions.include = [
 		{
 			model: tags,
 			attributes: ["text"],
 			as: "tags",
 		},
-	];
+	];*/
 	queryOptions.attributes = [
 		"id",
 		"name",
@@ -478,17 +491,17 @@ exports.searchRecipes = async (searchOptions) => {
 		"totalCarbs",
 		"totalFat",
 		"totalFiber",
-	]
+	];
 
-	let recipesList = await recipes.findAll(queryOptions)
-		.then(res => res)
-		.catch(err => {
+	let recipesList = await recipes
+		.findAll(queryOptions)
+		.then((res) => res)
+		.catch((err) => {
 			console.error(err);
 			throw new CustomError("Error while searching recipes", 500);
-		})
+		});
 
 	let recipesResponse = [];
-	for (let recipe of recipesList)
-		recipesResponse.push(recipe.get());
+	for (let recipe of recipesList) recipesResponse.push(recipe.get());
 	return recipesResponse;
-}
+};
